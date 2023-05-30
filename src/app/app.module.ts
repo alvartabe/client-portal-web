f8a288b6bfef7e72574082175d4aab9dd668b648
aartaviab <98897361+aartaviab@users.noreply.github.com>
2023-05-30 16:53:07 -0600

    template files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LucideAngularModule, File, Home, Menu, UserCheck, ChevronDown, Box } from 'lucide-angular';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LucideAngularModule.pick({File, Home, Menu, UserCheck, ChevronDown, Box})
   ],
   providers: [],
   bootstrap: [AppComponent]
