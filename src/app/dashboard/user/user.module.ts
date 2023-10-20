9b9cd7af1ebb6a0c002cd3a91f5a012866d0b8ed
alvartabe <alvartabe777@gmail.com>
2023-10-20 14:46:04 -0600

    ng update @angular/material@15

--- src/app/dashboard/user/user.module.ts
+++ src/app/dashboard/user/user.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { UserListComponent } from './user-list/user-list.component';
 import { SharedModule } from '@app/shared/shared.module';
 import { UserRoutingModule } from './user-routing.module';
-import { MatPaginatorModule} from '@angular/material/paginator';
+import { MatLegacyPaginatorModule as MatPaginatorModule} from '@angular/material/legacy-paginator';
 import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatButtonModule } from '@angular/material/button';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatLegacyTableModule as MatTableModule } from '@angular/material/legacy-table';
+import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
+import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import {MatLegacyMenuModule as MatMenuModule} from '@angular/material/legacy-menu';
 import { UserService } from './user.service';
 
 @NgModule({
